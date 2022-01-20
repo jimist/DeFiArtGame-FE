755fc6b8ec4692df82a864db168fcd678fe746bd
Amitoj Singh <amitojsingh366@gmail.com>
2022-01-21 00:12:31 +0300

    update api url

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,4 @@
 module.exports = {
     API_URL: process.env.REACT_APP_API_URL ?
-        process.env.REACT_APP_API_URL : 'https://18.189.90.223:3001',
+        process.env.REACT_APP_API_URL : 'https://api.defiartgame.com',
 }
